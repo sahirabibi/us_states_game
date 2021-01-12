bf6bc8cd63a40b6f4322bf55a0f6133d399e4f7a
Sahira Bibi <70290221+sahirabibi@users.noreply.github.com>
2021-01-12 10:08:43 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import pandas as pd 
+import turtle 
+from turtle import Turtle 
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "./blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+class State(Turtle):
+    def __init__(self, answer,coordinates):
+        """create a turtle text object with the answer if correct using name of state and tuple of coordinates"""
+        super().__init__()
+        self.hideturtle()
+        self.penup()  
+        self.goto(coordinates)      
+        self.write(arg=answer.title(), align='center', font=('Courier', 11, 'normal'))
+        
+
+# create a dataframe for states
+data = pd.read_csv('./50_states.csv')
+# isolate column of states as a list to check answer against 
+states_list = data.state.to_list()
+# get a tuple of x, y value from dataframe
+data['coordinates'] = list(zip(data.x, data.y))
+coordinates_list = data.coordinates.to_list()
+guessed_states = []
+game_on = True
+correct = 0  
+answer = screen.textinput(title=f"Guess a state {correct}/50", prompt = "Type a name of state.")
+while game_on: 
+    if answer.title() in states_list:
+        # get the index of the state in order to match with correct coordinates tuple 
+        match_index = states_list.index(answer.title())
+        # pass values to State class to generate turtle object
+        State(answer, coordinates_list[match_index])
+        # increase number of correct answers out of 50 by one
+        correct += 1
+        guessed_states.append(answer)
+        # print prompt screen again and ask for guess 
+        answer = screen.textinput(title=f"Guess a state{correct}/50", prompt = "Type a name of state.")    
+    elif answer.lower() == 'exit' or correct == 50:
+        turtle.write(f"Thanks for playing.\nTotal: {correct}/50", align='center',font=('Courier', 18, 'normal'))
+        unguessed_states = [state for state in states_list if state not in guessed_states]
+        game_on = False
+    else:
+        # incorrect, reprint prompt screen
+        answer = screen.textinput(title=f"Guess a state{correct}/50", prompt = "Type a name of state.")
+
+
+print(len(unguessed_states))
+screen.mainloop()
+
+
+# get coordinates of states 
+# def get_mouse_click_cor(x, y):
+#     print(x, y)
+
+# turtle.onscreenclick(get_mouse_click_cor)
+
